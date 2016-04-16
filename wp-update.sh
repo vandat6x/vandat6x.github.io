2c96b663ffbd61a0588cdb03ee5fae7df4ac784d
vandat6x <datvu.1407@gmail.com>
2016-04-16 18:43:57 +0700

    demo

--- wp-update.sh
+++ wp-update.sh
@@ -1,2 +1,4 @@
+
+read -p "Nhan [Enter] de tiep tuc ..."
 cd ${OPENSHIFT_REPO_DIR}www
  rm -rf wp-admin && rm -rf wp-includes && wget https://wordpress.org/latest.zip && unzip latest.zip && rm -rf wp-content && cp -r wordpress/* ${OPENSHIFT_REPO_DIR}www   && rm -rf wordpress && rm -rf latest.zip
