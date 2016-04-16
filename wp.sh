f3f4a69b7ca8a141e3c2afeb1830e442a34e6d6e
vandat6x <datvu.1407@gmail.com>
2016-04-16 18:05:29 +0700

    release v1

--- wp.sh
+++ wp.sh
@@ -1,2 +1,2 @@
 cd ${OPENSHIFT_REPO_DIR}www
-wget https://wordpress.org/latest.zip && unzip latest.zip && mv -- * .. && rm -rf wordpress && rm -rf latest.zip
+wget https://wordpress.org/latest.zip && unzip latest.zip && mv * ${OPENSHIFT_REPO_DIR}www  && rm -rf wordpress && rm -rf latest.zip
